e8d1c5d9fff553fd10e507cd87ac8642122273c3
sidonie <shelliednnguyen@gmail.com>
2021-03-12 18:19:39 -0800

    mysql connection

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require( './Product' );
+const Category = require( './Category' );
+const Tag = require( './Tag' );
+const ProductTag = require( './ProductTag' );
 
 // Products belongsTo Category
 
@@ -13,8 +13,8 @@ const ProductTag = require('./ProductTag');
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+   Product,
+   Category,
+   Tag,
+   ProductTag,
 };
