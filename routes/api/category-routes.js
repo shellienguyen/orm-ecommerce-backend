9c62f752c1f6ea848e543aefe83d0c9d4ad8c92a
sidonie <shelliednnguyen@gmail.com>
2021-03-14 20:12:53 -0700

    put routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,6 +44,19 @@ router.post( '/', ( req, res ) => {
 
 router.put( '/:id', ( req, res ) => {
    // update a category by its `id` value
+   Category.update(
+      {
+         category_name: req.body.category_name
+      },
+      {
+         where: { id: req.params.id }
+      }
+   )
+   .then( dbCategoryData => res.json( dbCategoryData ))
+   .catch( err => {
+      console.log( err );
+      res.status( 400 ).json( err );
+   } )
 });
 
 router.delete( '/:id', ( req, res ) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,7 +107,7 @@ router.put( '/:id', ( req, res ) => {
    })
    .then(( updatedProductTags ) => res.json( updatedProductTags ))
    .catch(( err ) => {
-      // console.log(err);
+      console.log(err);
       res.status( 400 ).json( err );
    });
 });
